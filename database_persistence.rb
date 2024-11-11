58b24b44be5e42e8ccbc90ce449e0e1522bd29ab
Esther Ho <esther.ho.ucd@gmail.com>
2024-11-10 16:58:35 -0800

    Optimize retrieval of todo counts when working with a single list
    - Use a single query to retrieve the count of total todos and uncompleted todos for a list
    - Include counts in the "list" hash

--- database_persistence.rb
+++ database_persistence.rb
@@ -42,13 +42,26 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+    SELECT lists.*,
+      COUNT(todos.id) AS todos_count,
+      COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count
+    FROM lists
+    LEFT JOIN todos ON lists.id = todos.list_id
+    WHERE lists.id = $1
+    GROUP BY lists.id
+    SQL
     result = query(sql, id)
 
     tuple = result.first
     list_id = tuple["id"].to_i
     todos = find_todos_for_list(list_id)
-    { id: list_id, name: tuple["name"], todos: todos }
+
+    { id: list_id,
+      name: tuple["name"],
+      todos: todos,
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 
   def update_list_name(id, new_name)
